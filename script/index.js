dbeec70a7f06f99c62e0ddafc059021c6bbf9675
SajibSaha207 <sajibsaha.207@gmail.com>
2025-09-06 11:21:41 +0600

    Added Modal functionality

--- script/index.js
+++ script/index.js
@@ -19,33 +19,53 @@ const loadLevelWord = (id) => {
         clickBtn.classList.add("active")
         displayLevelWord(data.data)});
 };
-const displayLevelWord = (words) => {
-    const wordContainer = document.getElementById("word-container");
-    wordContainer.innerHTML = "";
-    };
-
     const loadWordDetail = async (id) => {
         const url = `https://openapi.programming-hero.com/api/word/${id}`;
         const res = await fetch(url);
         const details = await res.json();
-        displayWordDetails(details);
+        displayWordDetail(details.data);
     };
 
-const displayloadWordDetail = (word) =>{
+const displayWordDetail = (word) =>{
+    console.log(word);
+    const detailsBox = document.getElementById("details-container");
+    detailsBox.innerHTML =`
+  <div class="">
+        <h2 class="text-2xl font-bold">${word.word}(<i class="fa-solid fa-microphone-lines"></i>${word.pronunciation}</h2>
+      </div>
+      <div class="">
+        <h2 class="font-bold">Meaning</h2>
+        <p>আগ্রহী</p>
+      </div>
+      <div class="">
+        <h2 class="font-bold">Example</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+      <div class="">
+        <h2 class="font-bold">Synonym</h2>
+        <span class="btn">Syn1</span>
+        <span class="btn">Syn2</span>
+        <span class="btn">Syn3</span>
+      </div>
+    `
+    document.getElementById("my_modal_5").showModal();
 
-}
-    if (words.length == 0) {
+};
+ 
+const displayLevelWord = (words) => {
+    const wordContainer = document.getElementById("word-container");
+    wordContainer.innerHTML = "";
+     if (words.length == 0) {
         wordContainer.innerHTML = `
         <div class="text-center   col-span-full rounded-x1 py-10 space-y-6 font-bangla">
-        <img class="mx-auto src="./assets/alert-error.png"/>
+        <img class="mx-auto src="./assets./alert-error.png"/>
   <p class="text-xl font-medium text-gray-400">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি। </p>
   <h2 class="font-medium text-4-xl">নেক্সট Lesson এ যান</h2>
 </div>
         `
         return;
     }
-
-    words.forEach((word) => {
+       words.forEach((word) => {
         console.log(word);
         const card = document.createElement("div");
         card.innerHTML =`
@@ -54,7 +74,7 @@ const displayloadWordDetail = (word) =>{
   <p class="font-semibold">Meaning / Pronunciation</p>
   <div class="text-2xl font-medium font-bangla">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"} / ${word.pronunciation ? word.pronunciation : "পাওয়া যায়নি"}"</div>
   <div class="flex justify-between items-center">
-    <button onclick=" loadWordDetail(${word.id}" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+    <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
     <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></i></button>
   </div>
 </div>
@@ -62,6 +82,7 @@ const displayloadWordDetail = (word) =>{
         wordContainer.append(card);
     });
 
+    };
 
 const displayLesson = (lessons) => {
     //1. get the container
